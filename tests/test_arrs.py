2606d2e951fb2448da6f085cdc0ea105d8c8e2fa
Andrey <bmw_740@mail.ru>
2023-04-17 01:00:43 +0500

    complited

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,6 +6,6 @@ def test_get():
     assert arrs.get([], 0, "test") == "test"
 
 
-def test_slice():
+def test_my_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
